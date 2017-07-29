bfc71997de1f6c594539b72d9e8bc8efa6cd31d0
Ali Salehi <alisalehi1995@gmail.com>
2017-07-29 19:05:16 +0430

    Updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'WPTavern',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'My first experimental Nuxt.js project using WPTavern REST API' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
